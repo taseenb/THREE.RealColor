f1f71e7cf90ffb23c22f0312fcbe8c6409e5573d
taseenb <esteban.almiron@gmail.com>
2017-10-02 05:22:17 +0100

    update

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Color class that extends THREE.Color with functions to deal with perceptual colors and interpolation.
 
+[image]: https://github.com/taseenb/THREE.RealColor/raw/master/realcolor-screenshot.jpg "THREE.RealColor"
--- dist/THREE.RealColor.js
+++ dist/THREE.RealColor.js
@@ -2722,6 +2722,68 @@ RealColor.prototype = Object.create(THREE.Color.prototype);
 RealColor.prototype.constructor = RealColor;
 
 /**
+ * Random
+ *
+ * Set the color to a random color.
+ */
+function random() {
+  this.setStyle(chroma.random());
+
+  return this;
+}
+
+/**
+ * Formatted
+ *
+ * Returns a formatted string with RGB values from 0 to 255.
+ * @param separator The separtor between the values. Default is a space.
+ */
+function formatted() {
+  var separator = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : ' ';
+
+  var c = this.to255();
+
+  return c.r + separator + c.g + separator + c.b;
+}
+
+/**
+ * To Array 255
+ *
+ * Same as toArray but with RGB values from 0 to 255
+ */
+function toArray255() {
+  return this.toArray().map(function (v) {
+    return Math.round(v * 255);
+  });
+}
+
+/**
+ * From Array 255
+ *
+ * Same as toArray but with RGB values from 0 to 255
+ */
+function fromArray255(array) {
+  this.r = array[0] / 255;
+  this.g = array[1] / 255;
+  this.b = array[2] / 255;
+
+  return this;
+}
+
+/**
+ * To 255
+ *
+ * Return an object with RGB values from 0 to 255
+ */
+function to255() {
+  return {
+    r: Math.round(this.r * 255),
+    g: Math.round(this.g * 255),
+    b: Math.round(this.b * 255)
+  };
+}
+
+/**
  * Mix
  *
  * Returns the linear blend of startColor and endColor, based on progress (value from 0 to 1).
@@ -2748,7 +2810,31 @@ function mix(targetColor, progress) {
   return this;
 }
 
+/**
+ * Mix From To
+ *
+ * Same as Mix but returns a new RealColor instance. It also requires a start color.
+ */
+function mixFromTo(startColor, targetColor, progress) {
+  var mode = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 'rgb';
+
+  var c = chroma.mix(startColor, targetColor, progress, mode).gl();
+
+  // {r: c[0], g: c[1], b: c[2]}
+
+  return new RealColor(c[0], c[1], c[2]);
+}
+
+// Methods
+RealColor.prototype.random = random;
+RealColor.prototype.formatted = formatted;
+RealColor.prototype.toArray255 = toArray255;
+RealColor.prototype.fromArray255 = fromArray255;
+RealColor.prototype.to255 = to255;
 RealColor.prototype.mix = mix;
+
+// Static methods
+RealColor.mixFromTo = mixFromTo;
 
 global.RealColor = RealColor;
 module.exports = RealColor;
